b9d3843707d71f94adf02b33c6f5a197035e015c
gregor <info@gregorkofler.com>
2025-02-20 12:32:53 +0100

    Replace vite plugin with postcss plugin and find md5 file observed.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { fileURLToPath, URL } from 'url'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
